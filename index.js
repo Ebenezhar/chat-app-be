6f89118df73d029834779618c7a1f2bbd0374f27
Ebenezhar <ebenezharselvakumar@gmail.com>
2022-10-10 23:22:56 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const dotenv = require("dotenv");
+app.use(cors());
+dotenv.config();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    },
+});
+
+io.on('connection', socket => {
+    const id = socket.handshake.query.id
+    socket.join(id)
+
+    socket.on('send-message', ({ recipients, text }) => {
+        recipients.forEach(recipient => {
+            const newRecipients = recipients.filter(r => r !== recipient)
+            newRecipients.push(id)
+            socket.broadcast.to(recipient).emit("receive-message", {
+                recipients: newRecipients, sender: id, text
+            })
+        })
+    })
+
+})
+server.listen(process.env.PORT || 3001);
